0967b33bef6f05fb25000f1a7c5ef7e63879c8cf
HosseinTajdini <hosseintajeddini000@gmail.com>
2025-05-11 23:16:03 +0330

    modified:   Simple Face Detection/base.py

--- Simple Face Detection/base.py
+++ Simple Face Detection/base.py
@@ -4,12 +4,14 @@ import pathlib
 cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_default.xml"
 
 clf=cv2.CascadeClassifier(str(cascade_path))
-camera=cv2.VideoCapture("test.mp4")
+camera=cv2.VideoCapture(0)
 
 
 while True:
     ret,frame=camera.read() 
-
+    if not ret:
+        print("Failed to grab frame")
+        break
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces=clf.detectMultiScale(
         gray,
@@ -25,5 +27,4 @@ while True:
     cv2.imshow("Faces",frame)
     if cv2.waitKey(1)==ord("q"):
         break
-camera.release()
-cv2.destroyAllWindows()    
+    
